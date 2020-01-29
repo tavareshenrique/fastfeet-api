52fa1fed566ebf2c52d13003b1366be0674b1aff
Henrique Tavares <ihenrits@gmail.com>
2020-01-29 00:50:54 -0300

    feat: creating models loader

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
+routes.get('/', async (req, res) => {
   return res.json({ message: 'Hello World' });
 });
 
