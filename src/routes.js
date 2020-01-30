b5812aecaada8d12b2e750ca496e5862ec1ee05f
Henrique Tavares <ihenrits@gmail.com>
2020-01-30 12:50:30 -0300

    feat: creating user controller and user store

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
