ee21257a9d786665622d681d5f97daaf1e5fe648
Henrique Tavares <ihenrits@gmail.com>
2020-01-31 01:50:50 -0300

    feat: store recipients

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,43 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Name is required'),
+      street: Yup.string().required('Street is required'),
+      number: Yup.string().required('Number is required'),
+      state: Yup.string().required('State is required'),
+      city: Yup.string().required('City is required'),
+      zipcode: Yup.string().required('ZIP Code is required'),
+    });
+
+    schema.validate(req.body, { abortEarly: false }).catch(e => {
+      return res.status(400).json({ error: e.errors });
+    });
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zipcode,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zipcode,
+    });
+  }
+}
+
+export default new RecipientController();
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -11,5 +12,7 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
