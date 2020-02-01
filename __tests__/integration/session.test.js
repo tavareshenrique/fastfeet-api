4db91140775349c576b1a1670f92db8041279369
Henrique Tavares <ihenrits@gmail.com>
2020-02-01 17:38:53 -0300

    test: testing session

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,13 +1,46 @@
+import request from 'supertest';
+
+import app from '../../src/app';
+import truncate from '../utils/truncate';
+
 import User from '../../src/app/models/User';
 
 describe('Authentication', () => {
-  it('should auth', async () => {
-    const userCreate = await User.create({
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should authenticate with valid credentials', async () => {
+    const user = await User.create({
       name: 'Henrique',
-      email: 'henrique@gmail.com',
+      email: 'ihenrits@gmail.com',
       password: '123456',
     });
 
-    expect(userCreate.email).toBe('ihenrits@gmail.com');
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: user.password,
+      });
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should return jwt token when authenticated', async () => {
+    const user = await User.create({
+      name: 'Henrique',
+      email: 'ihenrits@gmail.com',
+      password: '123456',
+    });
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: user.password,
+      });
+
+    expect(response.body).toHaveProperty('token');
   });
 });
