8ea45ed115c324d38b5fb5b5b50d4aa4f839fdd8
Henrique Tavares <ihenrits@gmail.com>
2020-02-01 18:26:05 -0300

    test: adding factories and faker in session.test

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -3,6 +3,8 @@ import request from 'supertest';
 import app from '../../src/app';
 import truncate from '../utils/truncate';
 
+import fakerRecipient from '../utils/faker/fakerRecipient';
+
 import factory from '../factories';
 
 describe('Authentication', () => {
@@ -47,5 +49,35 @@ describe('Authentication', () => {
       });
 
     expect(response.body).toHaveProperty('token');
+  });
+
+  it('should be able to access private routes when authenticated', async () => {
+    const user = await factory.create('User');
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: user.password,
+      });
+
+    expect(response.body).toHaveProperty('token');
+  });
+
+  it('should not be able to access private routes without jwt token', async () => {
+    const response = await request(app)
+      .post('/recipients')
+      .send(fakerRecipient);
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able to access private routes with invalid jwt token', async () => {
+    const response = await request(app)
+      .post('/recipients')
+      .send(fakerRecipient)
+      .set('Authorization', `Bearer 123123`);
+
+    expect(response.status).toBe(401);
   });
 });
