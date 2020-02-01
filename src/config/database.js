3bc9c96eb973a6fd357a3a53926362544009706f
Henrique Tavares <ihenrits@gmail.com>
2020-02-01 17:40:40 -0300

    chore: disable logging in database sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -9,6 +9,7 @@ module.exports = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   storage: './__tests__/database.sqlite',
+  logging: false,
   define: {
     timestamps: true,
     underscored: true,
