8855102e998073b0e1c8e25ab18f7cca96d1c783
Henrique Tavares <ihenrits@gmail.com>
2020-02-01 21:04:16 -0300

    test: removing recipient from factories

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -1,13 +1,9 @@
 import { factory } from 'factory-girl';
 
 import fakerUser from './utils/faker/fakerUser';
-import fakerRecipient from './utils/faker/fakerRecipient';
 
 import User from '../src/app/models/User';
-import Recipient from '../src/app/models/Recipient';
 
 factory.define('User', User, fakerUser);
-
-factory.define('Recipient', Recipient, fakerRecipient);
 
 module.exports = factory;
