62c78ebb251eb143097fe5e8d153c817ef561f62
Henrique Tavares <ihenrits@gmail.com>
2020-02-01 21:01:39 -0300

    test: update truncate

--- __tests__/utils/truncate.js
+++ __tests__/utils/truncate.js
@@ -1,7 +1,20 @@
-import User from '../../src/app/models/User';
-import Recipient from '../../src/app/models/Recipient';
+import database from '../../src/database';
 
-module.exports = async () => {
-  await User.destroy({ truncate: true, force: true });
-  await Recipient.destroy({ truncate: true, force: true });
-};
+// import User from '../../src/app/models/User';
+// import Recipient from '../../src/app/models/Recipient';
+
+export default function truncate() {
+  return Promise.all(
+    Object.keys(database.connection.models).map(key => {
+      return database.connection.models[key].destroy({
+        truncate: true,
+        force: true,
+      });
+    })
+  );
+}
+
+// module.exports = async () => {
+//   await User.destroy({ truncate: true, force: true });
+//   await Recipient.destroy({ truncate: true, force: true });
+// };
