a062de21034f91d4deac811364dbe071331c9971
Henrique Tavares <ihenrits@gmail.com>
2020-02-01 21:28:21 -0300

    refactor: add validators in routes

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,25 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 
+import validateUserStore from './app/validators/UserStore';
+import validateSessionStore from './app/validators/SessionStore';
+import validateRecipientStore from './app/validators/RecipientStore';
+import validateRecipientUpdate from './app/validators/RecipientUpdate';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/users', validateUserStore, UserController.store);
+routes.post('/sessions', validateSessionStore, SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.post('/recipients', RecipientController.store);
-routes.put('/recipients/:id', RecipientController.update);
+routes.post('/recipients', validateRecipientStore, RecipientController.store);
+routes.put(
+  '/recipients/:id',
+  validateRecipientUpdate,
+  RecipientController.update
+);
 
 export default routes;
