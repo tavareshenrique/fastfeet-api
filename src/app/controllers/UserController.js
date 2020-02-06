47a93d04c416d3ead4a1b20c60d8786a3f334276
Henrique Tavares <ihenrits@gmail.com>
2020-02-06 00:58:31 -0300

    feat: update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,6 +12,34 @@ class UserController {
 
     return res.json({ id, name, email });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ erro: 'Password does not match.' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
 }
 
 export default new UserController();
