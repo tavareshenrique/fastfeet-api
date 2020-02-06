24c3457fe41832dceed21c8034ae6697022f04a0
Henrique Tavares <ihenrits@gmail.com>
2020-02-06 00:58:15 -0300

    chore: add generateToken in User model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,8 @@
 import Sequelize, { Model } from 'sequelize';
+
 import bcrypt from 'bcryptjs';
+
+import jwt from 'jsonwebtoken';
 
 class User extends Model {
   static init(sequelize) {
@@ -27,6 +30,10 @@ class User extends Model {
 
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
+  }
+
+  generateToken() {
+    return jwt.sign({ id: this.id }, process.env.APP_SECRET);
   }
 }
 
