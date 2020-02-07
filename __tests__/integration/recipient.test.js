3a9ab07ddf2bfd1c24878e257a63f8e34763c4a2
Henrique Tavares <ihenrits@gmail.com>
2020-02-07 00:50:44 -0300

    test: testing recipient validators

--- __tests__/integration/recipient.test.js
+++ __tests__/integration/recipient.test.js
@@ -34,17 +34,17 @@ describe('Recipient', () => {
   it('should be able to update recipient when authenticated', async () => {
     const user = await factory.create('User');
 
-    const responseRecipients = await request(app)
+    const responseRecipientsPost = await request(app)
       .post('/recipients')
       .send(fakerRecipient)
       .set('Authorization', `Bearer ${user.generateToken()}`);
 
-    const response = await request(app)
-      .put(`/recipients/${responseRecipients.body.id}`)
+    const responseRecipientsPut = await request(app)
+      .put(`/recipients/${responseRecipientsPost.body.id}`)
       .send(fakerRecipient)
       .set('Authorization', `Bearer ${user.generateToken()}`);
 
-    expect(response.status).toBe(200);
+    expect(responseRecipientsPut.status).toBe(200);
   });
 
   it('should not be able to update recipient without authenticated', async () => {
@@ -76,5 +76,38 @@ describe('Recipient', () => {
       .set('Authorization', `Bearer 123`);
 
     expect(response.status).toBe(401);
+  });
+
+  it('should not register a recipient when field is required', async () => {
+    const user = await factory.create('User');
+    const { street } = fakerRecipient;
+
+    const response = await request(app)
+      .post('/recipients')
+      .send({
+        street,
+      })
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should not update a recipient when field is required', async () => {
+    const user = await factory.create('User');
+    const { street } = fakerRecipient;
+
+    const recipientPostResponse = await request(app)
+      .post('/recipients')
+      .send(fakerRecipient)
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    const response = await request(app)
+      .put(`/recipients/${recipientPostResponse.body.id}`)
+      .send({
+        street,
+      })
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(400);
   });
 });
