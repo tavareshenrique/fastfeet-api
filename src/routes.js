cd7548dd9501cc49081b3df6dedd31f0d4f8430d
Henrique Tavares <ihenrits@gmail.com>
2020-02-13 00:19:03 -0300

    chore: add File and Deliverymen in routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymenController from './app/controllers/DeliverymenController';
 
 import validateUserStore from './app/validators/UserStore';
 import validateUserUpdate from './app/validators/UserUpdate';
@@ -13,6 +17,7 @@ import validateRecipientUpdate from './app/validators/RecipientUpdate';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', validateUserStore, UserController.store);
 routes.post('/sessions', validateSessionStore, SessionController.store);
@@ -20,11 +25,16 @@ routes.post('/sessions', validateSessionStore, SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', validateUserUpdate, UserController.update);
+
 routes.post('/recipients', validateRecipientStore, RecipientController.store);
 routes.put(
   '/recipients/:id',
   validateRecipientUpdate,
   RecipientController.update
 );
+
+routes.post('/deliverymen', DeliverymenController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
