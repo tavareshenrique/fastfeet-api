9367acdca82ead63ae48aca4f9cb52b808213d36
Henrique Tavares <ihenrits@gmail.com>
2020-02-13 00:18:23 -0300

    test: update truncate file

--- __tests__/utils/truncate.js
+++ __tests__/utils/truncate.js
@@ -1,9 +1,13 @@
 import User from '../../src/app/models/User';
 import Recipient from '../../src/app/models/Recipient';
+import File from '../../src/app/models/File';
+import Deliveryman from '../../src/app/models/Deliveryman';
 
 module.exports = async () => {
   await User.destroy({ truncate: true, force: true });
   await Recipient.destroy({ truncate: true, force: true });
+  await File.destroy({ truncate: true, force: true });
+  await Deliveryman.destroy({ truncate: true, force: true });
 };
 
 // import database from '../../src/database';
