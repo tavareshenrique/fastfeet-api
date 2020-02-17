53545921ee2fd7d5077949556de21325efff87ed
Henrique Tavares <ihenrits@gmail.com>
2020-02-16 21:24:19 -0300

    test: integration test for deliveryman

--- __tests__/integration/deliveryman.test.js
+++ __tests__/integration/deliveryman.test.js
@@ -14,9 +14,63 @@ describe('Deliveryman', () => {
 
   it('should be register deliveryman when authenticated', async () => {
     const user = await factory.create('User');
+
     const response = await request(app)
       .post('/deliverymen')
       .send(fakerDeliveryman)
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should be list all deliverymen when authenticated', async () => {
+    await factory.create('Deliverymen');
+    const user = await factory.create('User');
+
+    const response = await request(app)
+      .get('/deliverymen')
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should be update deliverymen when authenticated', async () => {
+    const { id } = await factory.create('Deliverymen');
+    const user = await factory.create('User');
+
+    const response = await request(app)
+      .put(`/deliverymen/${id}`)
+      .send(fakerDeliveryman)
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should not be update deliverymen when user deliveryman already exists', async () => {
+    const deliveryman1 = await factory.create('Deliverymen', {
+      email: 'ihenrits@gmail.com',
+    });
+    const deliveryman2 = await factory.create('Deliverymen');
+
+    const user = await factory.create('User');
+
+    const response = await request(app)
+      .put(`/deliverymen/${deliveryman2.id}`)
+      .send({
+        name: deliveryman2.name,
+        email: deliveryman1.email,
+      })
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should be remove deliverymen when authenticated', async () => {
+    const { id } = await factory.create('Deliverymen');
+    const user = await factory.create('User');
+
+    const response = await request(app)
+      .delete(`/deliverymen/${id}`)
       .set('Authorization', `Bearer ${user.generateToken()}`);
 
     expect(response.status).toBe(200);
