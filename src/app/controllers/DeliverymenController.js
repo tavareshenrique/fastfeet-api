763b1ba87388c042efedfacf9ab4289d84f22b5b
Henrique Tavares <ihenrits@gmail.com>
2020-02-16 21:24:50 -0300

    chore: creating associate between File and Deliverymen

--- src/app/controllers/DeliverymenController.js
+++ src/app/controllers/DeliverymenController.js
@@ -1,6 +1,22 @@
 import Deliverymen from '../models/Deliverymen';
+import File from '../models/File';
 
 class DeliverymenController {
+  async index(req, res) {
+    const deliverymen = await Deliverymen.findAll({
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path'],
+        },
+      ],
+    });
+
+    return res.json(deliverymen);
+  }
+
   async store(req, res) {
     const { name, email } = await Deliverymen.create(req.body);
 
@@ -8,6 +24,52 @@ class DeliverymenController {
       name,
       email,
     });
+  }
+
+  async update(req, res) {
+    const { email } = req.body;
+    const { id } = req.params;
+
+    const deliveryman = await Deliverymen.findByPk(id);
+
+    if (email !== deliveryman.email) {
+      const userExists = await Deliverymen.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Deliveryman already exists.' });
+      }
+    }
+
+    await deliveryman.update(req.body);
+
+    const { id: idDeliveryman, name, avatar } = await Deliverymen.findByPk(id, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json({
+      id: idDeliveryman,
+      name,
+      email,
+      avatar,
+    });
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const deliverymen = await Deliverymen.findByPk(id);
+
+    await deliverymen.destroy();
+
+    return res.send();
   }
 }
 
