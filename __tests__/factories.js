e98ab108618df6a2c02eb1da71a70ea8283b5bf5
Henrique Tavares <ihenrits@gmail.com>
2020-02-16 21:24:08 -0300

    test: factorie for deliverymen

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -3,11 +3,17 @@ import { factory } from 'factory-girl';
 import faker from 'faker';
 
 import User from '../src/app/models/User';
+import Deliverymen from '../src/app/models/Deliverymen';
 
 factory.define('User', User, {
   name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
+});
+
+factory.define('Deliverymen', Deliverymen, {
+  name: faker.name.findName(),
+  email: faker.internet.email(),
 });
 
 module.exports = factory;
