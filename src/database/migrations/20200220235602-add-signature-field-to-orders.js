76962c536bddebba5bbde3ff18c8a84e05998561
Henrique Tavares <ihenrits@gmail.com>
2020-02-20 20:59:28 -0300

    feat: migration for add field signature in order

---
+++ src/database/migrations/20200220235602-add-signature-field-to-orders.js
@@ -0,0 +1,15 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('orders', 'signature_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'signatures', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.removeColumn('orders', 'signature_id');
+  },
+};
