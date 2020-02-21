707b91cfa913c16911779a23d9fe636779d96c3a
Henrique Tavares <ihenrits@gmail.com>
2020-02-20 23:38:12 -0300

    feat: configuring nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd97fb5a114318a',
+    pass: '86ced9d346e540',
+  },
+  default: {
+    from: 'Equipe Fastfeet <noreply@fastfeet.com>',
+  },
+};
