aede463da27715e0e5ef8e4afb6f98d9092509f5
Henrique Tavares <ihenrits@gmail.com>
2020-02-29 20:55:33 -0300

    chore: add cors in api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import './bootstrap';
 
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 
@@ -24,7 +26,18 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+    this.server.use(
+      '/signature',
+      express.static(
+        path.resolve(__dirname, '..', 'tmp', 'uploads', 'signatures')
+      )
+    );
   }
 
   routes() {
